90461965bba8389ffb651cc899b36f2ad568ec6c
Alexia <alexheg44@gmail.com>
2021-03-31 21:46:05 -0400

    commit tree 49bc86c9b4639cdd144c3cd97e3b623687dca46d

--- lib/nyc_api.rb
+++ lib/nyc_api.rb
@@ -10,9 +10,16 @@ class GetPrograms
     uri = URI.parse(URL)
     response = Net::HTTP.get_response(uri)
     response.body
-  end
-
 end
 
-programs = GetPrograms.new.get_programs
-puts programs
+def program_school
+  
+    programs = JSON.parse(self.get_programs)
+    programs.collect do |program|
+      program["agency"]  
+    end
+  end 
+
+
+#programs = GetPrograms.new.get_programs
+#puts programs
